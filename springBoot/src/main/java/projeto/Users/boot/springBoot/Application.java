dfc78264e5de8ae31dde97e01cf391b48fd7a803
VitordevJava <vitordev.java@gmail.com>
2025-01-29 20:27:03 -0300

    Criando entidade e implementando tabela no banco de dados com JPA e persistencia

--- springBoot/src/main/java/projeto/Users/boot/springBoot/Application.java
+++ springBoot/src/main/java/projeto/Users/boot/springBoot/Application.java
@@ -2,8 +2,10 @@ package projeto.Users.boot.springBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "projeto.Users.boot.model")
 public class Application {
 
 	public static void main(String[] args) {

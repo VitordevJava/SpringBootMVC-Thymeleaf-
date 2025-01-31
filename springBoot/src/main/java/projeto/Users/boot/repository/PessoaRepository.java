9314da3a241b6ac394a84f9857bdab9d8c00c97d
VitordevJava <vitordev.java@gmail.com>
2025-01-31 00:23:26 -0300

    Criando o primeiro cadastro com Spring Boot

---
+++ springBoot/src/main/java/projeto/Users/boot/repository/PessoaRepository.java
@@ -0,0 +1,15 @@
+package projeto.Users.boot.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import projeto.Users.boot.model.Pessoa;
+
+@Repository
+@Transactional
+public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
+
+	
+	
+}

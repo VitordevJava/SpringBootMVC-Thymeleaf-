9cdb716699b784c29a5e106f6879f8e373373a39
VitordevJava <vitordev.java@gmail.com>
2025-01-31 23:46:22 -0300

    Carregando nossa lista de pessoas cadastradas

--- springBoot/src/main/java/projeto/Users/boot/controller/PessoaController.java
+++ springBoot/src/main/java/projeto/Users/boot/controller/PessoaController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import projeto.Users.boot.model.Pessoa;
 import projeto.Users.boot.repository.PessoaRepository;
@@ -25,5 +26,13 @@ public class PessoaController {
 	public String salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
 		return "cadastro/cadastropessoa";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value ="/listapessoas" )
+	public ModelAndView pessoas(){ /*Define um método chamado pessoas que retorna um objeto ModelAndView*/
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa"); /*Cria um objeto ModelAndView que aponta para a view cadastro/cadastropessoa*/
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll(); /*Usa o pessoaRepository para buscar todas as pessoas cadastradas no banco de dados*/
+		andView.addObject("pessoas", pessoasIt); /*Adiciona a lista de pessoas (pessoasIt) ao ModelAndView com o nome "pessoas"*/
+		return andView; /*Retorna o objeto ModelAndView para o Spring MVC*/
 	}
 }

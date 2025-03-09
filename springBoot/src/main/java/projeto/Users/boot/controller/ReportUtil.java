1a40f97341ba0c38c53ddb315321bb04159d894a
VitordevJava <vitordev.java@gmail.com>
2025-03-08 21:29:04 -0300

    Criação da classe de geração do relatório

--- springBoot/src/main/java/projeto/Users/boot/controller/ReportUtil.java
+++ springBoot/src/main/java/projeto/Users/boot/controller/ReportUtil.java
@@ -1,5 +1,35 @@
 package projeto.Users.boot.controller;
 
-public class ReportUtil {
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
 
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletContext;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+@Component
+public class ReportUtil implements Serializable{
+
+	/*Retorna nosso PDF Em Byte para download no navegador*/
+	public byte[] gerarRelatorio (List listDados, String relatorio, ServletContext servletContext) throws Exception{
+		
+		/*Cria a lista de dados para o relatorio com a nossa lista de objetos para imprimir*/
+		JRBeanCollectionDataSource JrBCDS = new JRBeanCollectionDataSource(listDados);
+		
+		/*Carregar o caminho do noosso arquivo Jasper compilado*/
+		
+		String caminhoJasper = servletContext.getRealPath("relatorios") + File.separator + relatorio + ".jasper";
+		
+		/*Carrega o arquivo Jasper passando os dados*/
+		
+		JasperPrint impressoraJasper = JasperFillManager.fillReport(caminhoJasper, new HashMap(), JrBCDS);
+		
+		return JasperExportManager.exportReportToPdf(impressoraJasper);
+	}
 }
